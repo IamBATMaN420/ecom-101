1a2186cba66376769da02065a0ed88d6e01a72d4
helper.dev <basakayush942@gmail.com>
2025-06-11 19:45:23 +0530

    otp controller added

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,6 +1,20 @@
+import { sendOtp } from "../config/sendOtp.js";
 import { TryCatch } from "../config/trycatch.js"
+import { otpModel } from "../models/otpSchema.js";
 
 export const loginUser = TryCatch(async (req, res) => {
   const { email } = req.body;
-  res.json(email)
+  const subject = "Ecommerce App"
+  const otp = Math.floor(Math.random() * 1000000)
+  const prvOtp = otpModel.findOne({
+    email,
+  })
+  if (prvOtp) {
+    await prvOtp.deleteOne()
+  }
+  await sendOtp({ email, subject, otp })
+  await otpModel.create(email, otp)
+  res.json({
+    message:"OTP will be send to Email"
+  })
 })
